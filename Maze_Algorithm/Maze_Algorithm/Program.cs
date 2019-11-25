45b8e2db2f12049eb975db4ff9e4655558365e40
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2019-11-25 14:53:02 -0600

    Maze Class Added

--- Maze_Algorithm/Maze_Algorithm/Program.cs
+++ Maze_Algorithm/Maze_Algorithm/Program.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace Maze_Algorithm
 {
+    /// <summary>
+    /// Maze Algorithm will take in a number to determine the size of the maze. 
+    /// From there it'll create that many rooms and randomly assign a minimum amount of connects that need to be made to make the maze complete
+    /// Since each maze room will be a square these connections will be on either the left, right, top, or bottom sides.
+    /// To make sure the maze is realistic it'll check if any rooms overlap and recreate until otherwise.
+    /// When completed, it will print out a list of every connect between rooms
+    /// With the format of Room Number and Doorway <-> Room Number and Doorway
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
         }
     }
